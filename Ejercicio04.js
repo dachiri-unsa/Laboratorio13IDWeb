351a82d37f597f348a9fb9c1d1d6000ac2804a5d
dachiri-unsa <dachiri@unsa.edu.pe>
2025-10-29 14:51:55 -0500

    Avance

--- Ejercicio04.js
+++ Ejercicio04.js
@@ -1,3 +1,15 @@
 const filtrarYTransformar = (arreglo) => {
-
+    arreglo = arreglo.filter(num => num >= 0 )
+    let sumatoria = 0
+    arreglo.forEach((num, i) => {
+         sumatoria+=num*num
+    })
+    return sumatoria
 }
+
+const arreglo_numeros = [3,-2,5,-9,0]
+console.log(
+    "El arreglo es: "+arreglo_numeros+
+    "\nLa respuesta de la suma de los cuadrados de los positivos es: "+
+    filtrarYTransformar(arreglo_numeros)
+)
